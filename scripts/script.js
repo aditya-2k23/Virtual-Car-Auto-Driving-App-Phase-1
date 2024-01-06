2300deaa6a65f5a0809ea9f965ef3e141d927eda
Aditya <holaaditya123@gmail.com>
2024-01-06 12:29:36 +0530

    defining te best car

--- scripts/script.js
+++ scripts/script.js
@@ -30,11 +30,13 @@ function animate(time) {
   for (let i = 0; i < cars.length; i++) {
     cars[i].update(road.borders, traffic);
   }
+  const bestCar = cars.find((c) => c.y == Math.min(...cars.map((c) => c.y)));
+
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
 
   carCtx.save();
-  carCtx.translate(0, -cars[0].y + carCanvas.height * 0.7);
+  carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7);
 
   road.draw(carCtx);
   for (let i = 0; i < traffic.length; i++) {
@@ -46,11 +48,11 @@ function animate(time) {
     cars[i].draw(carCtx, "blue");
   }
   carCtx.globalAlpha = 1;
-  cars[0].draw(carCtx, "blue", true);
+  bestCar.draw(carCtx, "blue", true);
 
   carCtx.restore();
 
   networkCtx.lineDashOffset = -time / 50;
-  Visualizer.drawNetwork(networkCtx, cars[9].brain);
+  Visualizer.drawNetwork(networkCtx, bestCar.brain);
   requestAnimationFrame(animate);
 }
